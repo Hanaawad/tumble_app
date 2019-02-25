79c133df5a3bef9aa73d8f7a46424933f4c8cb47
Max Glasmacher <maxglasmacher@gmail.com>
2019-02-25 13:51:00 +0100

    seed users no photos yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p "user 1"
+nic = User.new(name: "Nicolas", surname: "Feer", email: "nic@gmail.com", password: "nic12345",
+  age: 19, location: "Copenhagen", description: "Hi I'm Nic. I'm currently on my gap year before starting uni at Masstricht in Sep.
+  I love karaoke and gaming!")
+nic.photo =
+p "user 2"
+chris = User.new(name: "Christian", surname: "Soelling", email: "chris@gmail.com", password: "chris12345",
+  age: 18, location: "Singapore", description: "Im Chris, originally from Copenhagen. I love Fifa and traveling!")
+chris.photo =
+p "user 3"
+lucas = User.new(name: "Lucas", surname: "Cheung", email: "lucas@gmail.com", password: "lucas12345",
+  age: 17, location: "Buenos Aires", description: "Love climbing, basketball and asian food :)")
+lucas.photo =
+p "user 4"
+hana = User.new(name: "Hana", surname: "Awad", email: "hana@gmail.com", password: "hana12345",
+  age: 19, location: "Cairo", description: "Hi! I love cooking and doing all sorts of sports, especially surfing!")
+hana.photo =
+p "user 5"
+max = User.new(name: "Max", surname: "Glasmacher", email: "max@gmail.com", password: "max12345",
+  age: 18, location: "Buenos Aires", description: "Yoo! Surfing, basketball and traveling is ma life!!!")
+max.photo =
+p "user 6"
+amanda = User.new(name: "Amanda", surname: "Schjørmann", email: "amanda@gmail.com", password: "amanda12345",
+  age: 18, location: "Copenhagen", description: "Heya! I always love skydiving and finding new cool bars!")
+amanda.photo =
+p "user 7"
+irene = User.new(name: "Irene", surname: "Jona Smith", email: "irene@gmail.com", password: "irene12345",
+  age: 19, location: "Cairo", description: "Hii! Really into coding, climbing and rugby!")
+irene.photo =
+p "user 8"
+mohamed = User.new(name: "Mohamed", surname: "Awad", email: "mohamed@gmail.com", password: "mohamed12345",
+  age: 21, location: "Copenhagen", description: "Yoyo whatsup! Im a Microsoft intern traveling the world!
+  I love cooking, catan and going on crazy adventures!")
+mohamed.photo =
+p "user 9"
+ana = User.new(name: "Ana", surname: "Ivan", email: "ana@gmail.com", password: "ana12345",
+  age: 19, location: "Singapore", description: "Hihi! Currently travlin the world before becoming a professional snooker player
+  next year! I loove food and new cultures!")
+ana.photo =
+p "user 10"
+andre = User.new(name: "Andre", surname: "Orvalho", email: "andre@gmail.com", password: "andre12345",
+  age: 19, location: "Singapore", description: "Hihi! gyming, skating and surfing are liiiit!!")
+andre.photo =
+
