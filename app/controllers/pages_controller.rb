17b7be893134e97886e5659dcfb9d21c6163a35f
Hana Awad <hanaawad_92@hotmail.com>
2019-02-26 19:56:33 +0100

    still redesigning

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
 
   def home
     @most_recent_activity = Activity.order("activation_date").first
-    @activities = Activity.all
+    @top_three_activity = Activity.all.sample(3)
   end
 
   def search
