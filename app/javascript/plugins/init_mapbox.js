55b04fcf06933519ef272f1cbe58a992d5978d58
Max Glasmacher <maxglasmacher@gmail.com>
2019-02-27 09:36:12 +0100

    map half

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,9 +1,13 @@
 import mapboxgl from 'mapbox-gl';
 
-
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+};
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
@@ -13,18 +17,12 @@ const initMapbox = () => {
     });
   }
     const markers = JSON.parse(mapElement.dataset.markers);
-      markers.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(map);
+  markers.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(map);
   });
   fitMapToMarkers(map, markers);
-};
-
-const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
 };
 
 export { initMapbox };
