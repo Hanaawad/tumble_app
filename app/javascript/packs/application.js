5e1fd161419dafff52b8f0b4f6a2e07289129154
Hana Awad <hanaawad_92@hotmail.com>
2019-02-28 17:52:05 +0100

    cards center done, still hovering

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,9 +6,11 @@ import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
 
-balapaCop("Bouncing Profile Card", "rgba(255,255,255,.5)");
+// balapaCop("Bouncing Profile Card", "rgba(255,255,255,.5)");
 
 
 
-
+$("#show-map").click(function(){
+  $("#map").toggleClass("hide")
+});
 
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,7 +1,22 @@
 import mapboxgl from 'mapbox-gl';
 
-const initMapbox = () => {
-  const mapElement = document.getElementById('map');
+const mapElement = document.getElementById('map');
+
+const buildMap = () => {
+  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+  return new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/hanaawad/cjser19tb23ha1fnbwt14f1xv'
+  });
+};
+
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([ marker.lng, marker.lat ])
+      .addTo(map);
+  });
+};
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
@@ -9,23 +24,11 @@ const fitMapToMarkers = (map, markers) => {
   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
 };
 
-  if (mapElement) { // only build a map if there's a div#map to inject into
-    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
-    });
+const initMapbox = () => {
+  if (mapElement) {
+    const map = buildMap();
     const markers = JSON.parse(mapElement.dataset.markers);
-<<<<<<< HEAD
-=======
-
-
->>>>>>> master
-    markers.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(map);
-    });
+    addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
   }
 };
