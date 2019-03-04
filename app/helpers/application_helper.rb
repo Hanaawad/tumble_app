0d6bb3ef96668680688a97ff10a303099034e8b5
Max Glasmacher <maxglasmacher@gmail.com>
2019-03-04 12:28:44 +0100

    first changes done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def generate_navbar
+    if params["controller"] == "pages" && params["action"] == "home"
+      return "home-navbar"
+    else
+      return "normal-navbar"
+    end
+  end
 end
