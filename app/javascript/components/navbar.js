57da55fc83d84a1259f8e168634208b013e6bde5
Max Glasmacher <maxglasmacher@gmail.com>
2019-03-04 15:15:56 +0100

    done navbar

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.normal-navbar-wagon');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= (window.innerHeight - 400)) {
         navbar.classList.add('normal-navbar-wagon-white');
       } else {
         navbar.classList.remove('normal-navbar-wagon-white');
@@ -11,4 +11,5 @@ const initUpdateNavbarOnScroll = () => {
   }
 };
 
-export { initUpdateNavbarOnScroll };
+
+export { initUpdateNavbarOnScroll, initUpdateHomeNavbarOnScroll };
