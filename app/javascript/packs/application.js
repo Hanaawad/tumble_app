04c3fc6e80e93faf8fdd90d448351b7a76ba50c7
Max Glasmacher <maxglasmacher@gmail.com>
2019-03-04 14:06:01 +0100

    nav changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,13 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import "../plugins/flatpickr";
+import { initUpdateNavbarOnScroll } from '../components/navbar';
  // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
+
+initUpdateNavbarOnScroll();
 
 // balapaCop("Bouncing Profile Card", "rgba(255,255,255,.5)");
